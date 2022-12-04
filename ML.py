4c3cc438f019d2b4bec276aebd332e62839e7cb5
danushkgc <danushkgc@gmail.com>
2022-12-04 11:20:01 +0530

    1st commit

---
+++ ML.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+name_list = ['Danushka', 'Taniya',  'Kalalpitiya', 'Jayasanka','Milan']
+
+for name in name_list:
+    print(name, " is a student in Python class")
+
+
+# In[1]:
+
+
+def mlfunc(x):
+    y = x+2
+    return y
+
+
+# In[5]:
+
+
+def mlfunc1(x):
+    y = x**2
+    return y
+
+
+# In[ ]:
+
+
+
+
